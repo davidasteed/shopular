3873f67748ebe87ade0e481dd03fddc92ef87d28
David Steed <davidasteed@gmail.com>
2017-03-27 12:44:44 -0400

    first commit

---
+++ src/js/shop.controller.js
@@ -0,0 +1,10 @@
+(function() {
+  'use strict';
+
+  // create new Angular controller
+  angular.module('shop')
+    .controller('ShopController', ShopController);
+
+  
+
+}());
---
+++ src/js/shop.module.js
@@ -0,0 +1,8 @@
+(function() {
+  'use strict';
+
+  // create new Angular module
+  angular.module('shop', []);
+
+
+}());
